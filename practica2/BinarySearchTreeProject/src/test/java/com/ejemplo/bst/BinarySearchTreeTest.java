88ec339df1b883d159e82323d45e86984014e8ad
maxiprados <160849653+maxiprados@users.noreply.github.com>
2025-03-27 12:08:15 +0100

    Primer Test

--- practica2/BinarySearchTreeProject/src/test/java/com/ejemplo/bst/BinarySearchTreeTest.java
+++ practica2/BinarySearchTreeProject/src/test/java/com/ejemplo/bst/BinarySearchTreeTest.java
@@ -1,16 +1,44 @@
 package com.ejemplo.bst;
 
 import com.ejemplo.bst.BinarySearchTree;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import java.util.Comparator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class BinarySearchTreeTest {
-    Comparator<Integer> comparator = Integer::compareTo;
-    @Test
-    public void testAdd() {
-        BinarySearchTree bst = new BinarySearchTree<>(comparator);
+    
+    
 
+
+
+
+    @Nested
+    class removeBranchClass{
+        @Test
+        @DisplayName("Limpiar rama con un nodo hoja")
+        public void arbolConRamaHoja_seElimina(){
+            //Arrange
+            Comparator<Integer> comparator = Integer::compareTo;
+            BinarySearchTree<Integer> tree = new BinarySearchTree<>(comparator);
+            tree.insert(50);
+            tree.insert(30);
+            tree.insert(70);
+            String expected = "50(30,)";
+
+            //Act: Eliminamos cualquier rama con nodo hoja
+            tree.removeBranch(70);
+
+            //Assert: Nos aseguramos de haber eliminado 
+            assertFalse(tree.contains(70));
+            assertEquals(expected, tree.render());
+
+        }
     }
 
 }
