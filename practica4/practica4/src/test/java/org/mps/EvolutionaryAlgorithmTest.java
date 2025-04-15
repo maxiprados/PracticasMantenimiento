339de2c19f7628205eb6665b739f6056db548fb6
maxiprados <160849653+maxiprados@users.noreply.github.com>
2025-04-15 12:21:42 +0200

    EvolutionaryAlgorithm - Tests
    Constructor realizado
    Quedan el resto de esta clase

--- practica4/practica4/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
+++ practica4/practica4/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
@@ -1,5 +1,43 @@
 package org.mps;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mps.mutation.*;
+import org.mps.crossover.*;
+import org.mps.selection.*;
+
 public class EvolutionaryAlgorithmTest {
     
+    @Test
+    @DisplayName("Test EvolutionaryAlgorithm - Constructor con parámetros nulos")
+    public void evolutionaryAlgorithm_ConstructorParametrosNulos_ThrowsException(){
+        // Arrange
+        SelectionOperator selectionOperator = null;
+        MutationOperator mutationOperator = null;
+        CrossoverOperator crossoverOperator = new TwoPointCrossover();
+        
+        // Act & Assert
+        assertThrows(EvolutionaryAlgorithmException.class,() -> {
+            new EvolutionaryAlgorithm(selectionOperator, mutationOperator, crossoverOperator);
+        });
+    }
+    
+    @Test
+    @DisplayName("Test EvolutionaryAlgorithm - Constructor con parámetros válidos")
+    public void evolutionaryAlgorithm_ConstructorParametrosValidos_NoLanzaExcepcion() throws EvolutionaryAlgorithmException{
+        // Arrange
+        SelectionOperator selectionOperator = new TournamentSelection(2);
+        MutationOperator mutationOperator = new GaussianMutation();
+        CrossoverOperator crossoverOperator = new TwoPointCrossover();
+        
+        // Act & Assert
+        try {
+            new EvolutionaryAlgorithm(selectionOperator, mutationOperator, crossoverOperator);
+        } catch (EvolutionaryAlgorithmException e) {
+            // Si se lanza una excepción, la prueba falla
+            assert false;
+        }
+    }
 }
